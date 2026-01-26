db8112132e125f28831cac0f9a32ac3f32e4eb44
Ryan Bradshaw <bradshr@bc.edu>
2026-01-26 15:06:47 -0500

    Aftyer Lesson 1.13.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message = ""
@@ -15,6 +16,10 @@ struct ContentView: View {
     @State private var lastMessageNumber = 0
     @State private var lastImageNumber = 0
     @State private var counter2 = 0
+    let numberOfImages = 9
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var lastSoundNumber = -1
+    let numberofSounds = 6
     
     @State private var messages : [String] = ["Led Zeppelin IV", "Random Access Memories by Daft Punk, featuring Pharrell Williams", "Dirt", "American Football", "Revolver","Van Halen", "Raise!", "Dookie", "Wish You Were Here", "Who's Next" ]
     var body: some View {
@@ -27,7 +32,7 @@ struct ContentView: View {
                 .minimumScaleFactor(0.5)
                 .frame(height: 120)
                 .animation(.easeInOut(duration: 0.15),value:message)
-            
+            Spacer()
             Image(imageName)
                 .resizable()
                 .scaledToFit()
@@ -47,13 +52,28 @@ struct ContentView: View {
                     lastMessageNumber = counter
                     message = messages[counter]
                     repeat{
-                        counter2 = Int.random(in: 0...9)
+                        counter2 = Int.random(in: 0...numberOfImages)
                     } while counter2 == lastImageNumber
                     lastImageNumber = counter2
                     imageName = "image\(counter2)"
                     
+                    var soundNumber: Int
+                    repeat{
+                        soundNumber = Int.random(in: 0...numberofSounds-1)
+                    } while soundNumber == lastSoundNumber
+                    lastSoundNumber = soundNumber
+                    let soundName = "sound\(soundNumber)"
                     
-                    
+                    guard let soundFile = NSDataAsset(name: soundName) else{
+                        print("🤬 Could not read file named \(soundName)")
+                        return
+                    }
+                    do{
+                        audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                        audioPlayer.play()
+                    } catch {
+                        print("Error: \(error.localizedDescription) creating audioPlayer.")
+                    }
                     
                     
                 }
