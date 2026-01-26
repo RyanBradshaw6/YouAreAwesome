3feaa45879ff77e3a7b4c3d0044bb33c02bd9b86
Ryan Bradshaw <bradshr@bc.edu>
2026-01-26 12:51:01 -0500

    After 1.11 Lesson

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -13,36 +13,33 @@ struct ContentView: View {
     @State private var imageNumber = 0
     @State private var counter = 0
 
-    @State private var messages : [String] = ["Led Zeppelin IV", "Random Access Memories", "Dirt", "American Football", "Revolver","Van Halen", "Raise!", "Dookie", "Wish You Were Here", "Who's Next" ]
+    @State private var messages : [String] = ["Led Zeppelin IV", "Random Access Memories by Daft Punk, featuring Pharrell Williams", "Dirt", "American Football", "Revolver","Van Halen", "Raise!", "Dookie", "Wish You Were Here", "Who's Next" ]
     var body: some View {
         VStack {
-            Spacer()
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .clipShape(.rect(cornerRadius: 20))
-                .shadow(radius: 20)
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
                 .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 120)
+                .animation(.easeInOut(duration: 0.15),value:message)
+
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(.rect(cornerRadius: 20))
+                .shadow(radius: 20)
+                .animation(.default,value:imageName)
+            
             Spacer()
             HStack{
                 
-                Button("Press Me!") {
-                    
+                Button("Show Message") {
+                    counter = Int.random(in: 0..<messages.count)
                     message = messages[counter]
-                    counter += 1
-                    if counter == messages.count {
-                        counter = 0
-                    }
-                    imageName = "image\(imageNumber)"
-                    imageNumber += 1
-                    if imageNumber > 9 {
-                        imageNumber = 0
-                        
-                    }
+                    imageName = "image\(Int.random(in: 0...9))"
+                    
                     
                     
                     
